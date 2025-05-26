ef6203698155ac1122eb836bea507a9595e2475d
CosmicTantoFan897432 <127630095+jnbfondiscord@users.noreply.github.com>
2025-05-26 12:13:36 +0300

    Delete public directory

--- public/script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-window.addEventListener('DOMContentLoaded', () => {
-  const msg = document.getElementById('message');
-  msg.innerHTML = `
-    <h1>Hello, Web Browser!</h1>
-    <p>You are viewing this via a browser with JS enabled.</p>
-  `;
-});
