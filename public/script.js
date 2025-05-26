0958894a85ebace5e93adbcf36165ed576b32561
CosmicTantoFan897432 <127630095+jnbfondiscord@users.noreply.github.com>
2025-05-26 12:08:38 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 // script.js
 window.addEventListener('DOMContentLoaded', () => {
   const msg = document.getElementById('message');
-  // Overwrite the default for real browsers
   msg.innerHTML = `
     <h1>Hello, Web Browser!</h1>
     <p>You are viewing this via a browser with JS enabled.</p>
